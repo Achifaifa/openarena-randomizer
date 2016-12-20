d84e5f93e81711eed47dec4cd34012d88fabb805
Yuri Numerov <achi@hush.ai>
2016-12-21 00:35:33 +0100

    fixed seta settings

--- genconfig.py
+++ genconfig.py
@@ -9,45 +9,48 @@ yesno=lambda:random.choice([0,1])
 configfile=open("./out.cfg","w+")
 
 # Fixed settings
-configfile.write("seta sv_hostname ^1Ach^8ifa^3ifa^2nta^4sti^6co! \n")
-configfile.write("seta sv_pure 1\n")
-configfile.write("seta timelimit 0\n")
-configfile.write("seta fraglimit %s\n"%raw_input("Fraglimit?\n>"))
-
-# Select mode
-ir=raw_input("Choose [I]nstagib, [R]ockets or [N]one\n>").lower()
-if ir=="i": configfile.write("seta g_instantgib 1\n")
-elif ir=="r": configfile.write("seta g_rockets 1\n")
-
-# Vampire and health settings
-if raw_input("Randomize vampire and health settings? (y/n)\n>")=="y":
-  configfile.write("seta g_vampire %i\n"%yesno())
-  configfile.write("seta g_vampire_max_health %i\n"%random.randint(200,600))
-  configfile.write("seta g_regen %f\n"%(random.randint(0,50)/10.))
-  configfile.write("seta g_quadfactor %f\n"%(random.randint(10,50)/10.))
-  configfile.write("seta g_damagemodifier %i\n"%random.randint(10,30)/10.)
-
-# Misc game settings
-if raw_input("Randomize misc settings? (y/n)\n>")=="y":
-  configfile.write("seta g_awardpushing %i\n"%yesno())
-  configfile.write("seta g_speed %i\n"%random.randint(220,420))
-  configfile.write("seta g_knockback %i\n"%random.randint(800,1200))
-  configfile.write("seta g_gravity %i\n"%random.randint(650,950))
-  configfile.write("seta g_respawntime %i\n"%random.randint(1,5))
+configfile.write("set sv_hostname ^1Ach^8ifa^3ifa^2nta^4sti^6co!\n")
+configfile.write("set sv_motd 'Con 7 portaminas y hierro'")
+configfile.write("set sv_maxclients 16\n")
+configfile.write("set sv_pure 0\n")
+configfile.write("set timelimit 0\n")
+configfile.write("set fraglimit %s\n"%raw_input("Fraglimit?\n>"))
 
 # Game mode setting
 print "Pick the game mode"
 print "\n".join("%i - %s"%(i,s) for i,s in enumerate(gamemodes.keys()))
-configfile.write("seta g_gametype %s\n"%gamemodes[gamemodes.keys()[int(raw_input(">"))]])
+configfile.write("set g_gametype %s\n"%gamemodes[gamemodes.keys()[int(raw_input(">"))]])
+
+# Select mode
+ir=raw_input("Choose [I]nstagib, [R]ockets or [N]one\n>").lower()
+if ir=="i": configfile.write("set g_instantgib 1\n")
+elif ir=="r": configfile.write("set g_rockets 1\n")
+else: configfile.write("set g_rockets 0\nset g_rockets 0\n")
+
+# Vampire and health settings
+if raw_input("Randomize vampire and health settings? (y/n)\n>")=="y":
+  if yesno(): configfile.write("set g_vampire %f\n"%(random.randint(0,100)/80.))
+  configfile.write("set g_vampire_max_health %i\n"%random.randint(100,600))
+  if yesno(): configfile.write("set g_regen %f\n"%(random.randint(0,50)/10.))
+
+# Misc game settings
+if raw_input("Randomize misc settings? (y/n)\n>")=="y":
+  configfile.write("set g_awardpushing %i\n"%yesno())
+  configfile.write("set g_speed %i\n"%random.randint(250,420))
+  configfile.write("set g_knockback %i\n"%random.randint(700,1200))
+  configfile.write("set g_gravity %i\n"%random.randint(650,950))
+  configfile.write("set g_respawntime %i\n"%random.randint(1,10))
+  configfile.write("set g_quadfactor %f\n"%(random.randint(10,50)/10.))
+  configfile.write("set g_damagemodifier %f\n"%(random.randint(5,30)/10. if yesno() else 1.))
 
 # Randomize removal of items from arena
-if raw_input("Randomize item restrictions? (y/n)\n>")=="y":
-  for i in weaponres:
-    configfile.write("seta disable_%s '%i'\n"%(i,yesno()))
+randitems=raw_input("Randomize item restrictions? (y/n)\n>").lower()
+for i in weaponres:
+  configfile.write("set disable_%s '%i'\n"%(i,yesno() if randitems=="y" else 0))
 
-
-  
-
+# Temporary settings (Bots for testing, etc)
+configfile.write("set bot_minplayers 4")
+configfile.write("set bot_nochat 1")
 
 
 configfile.close()
